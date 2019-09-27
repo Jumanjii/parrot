2f6f181e966cd716dc4ffe2078aa349f4ca3f10e
Allan Jacquet-Cretides <Jumanjii@users.noreply.github.com>
2019-09-28 00:35:08 +0200

    update title html

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ const headerBulma = `
 	<head>
 	    <meta charset="utf-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
-	    <title>Momo</title>
+	    <title>Parrot</title>
 	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
 	    <script defer src="https://use.fontawesome.com/releases/v5.4.0/js/all.js"></script>
 	</head>
